525afb1219d7a9f7cfa4e6d35c2b7a65dd2a9072
Adrian Mohmed Sanz <adrian.mohmed@kubide.es>
2021-08-25 15:19:55 +0200

    parsing csv file

--- src/data/controller/data.controller.ts
+++ src/data/controller/data.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Inject, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { DataService } from '../service/data.service';
 
@@ -9,10 +9,14 @@ export class DataController {
     ) {}
     
     @Post()
-    @UseInterceptors(FileInterceptor('file'))
+    @UseInterceptors(FileInterceptor('file', {
+        dest: './files',
+        preservePath: true
+    }))
     async postFile(
         @UploadedFile() file: any
     ): Promise<void> {
-       this.dataService.handleFile(file);
+       const fileContent = await this.dataService.handleFile(file);
+       return fileContent;
     }
 }
--- src/data/service/data.service.ts
+++ src/data/service/data.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@nestjs/common';
-
+import * as csv from 'csv-parser';
+import * as fs from 'fs';
 @Injectable()
 export class DataService {
-    public handleFile(file: File): void {
+    public async handleFile(file: File): Promise<any> {
         console.log(file);
+        const fileContent = await this.parseCSV(file);
+
+        return fileContent;
     }
+
+    private async parseCSV(file: File) {
+        const results = [];
+        const files = fs.readdirSync('./files');
+        console.log(files);
+        fs.createReadStream(`./files/${files[0]}`)
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => console.log('parsing ended'))
+        
+        return results;
+    }
+
+    private async seeReport(table: object[]) {
+        return table;
+    } 
 }
